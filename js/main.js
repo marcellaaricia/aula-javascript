6c552b04871102eed9109df05bb3128d2bb429b7
Marcella Arícia <marcella.aricia@live.com>
2020-11-24 01:24:47 -0300

    -

---
+++ js/main.js
@@ -0,0 +1,90 @@
+function clicou(){
+document.getElementById("agradecimento").innerHTML = "Obrigada por clicar";
+    //alert("Obrigada por clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/marcellaaricia");
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse";
+    elemento..innerHTML = "Obrigada por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+/* function soma(n1, n2){
+    return n1 + n2;
+}
+var validar = 0;
+
+function validadeIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    } else{
+        validar = false
+    }
+    return validar
+}
+
+var idade = prompt("Qual sua idade?") */
+
+/*function setReplace(frase, name, novo_name){
+    return frase.replace(name, novo_name)
+}
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));*/
+
+
+/* var d = new Date();
+alert(d); //MOSTRA A DATA ATUAL
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes()); */
+
+/* var count;
+for(count=0; count <= 5; count++)[
+    alert(count);
+]*/
+
+/* var count = 5;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+} */
+
+/* var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("menor de idade");
+}; */
+
+/* var nome = "Marcella Arícia";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos"); 
+alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil")); */
+
+/* var lista = ["maça", "pêra", "laranja"];
+lista.push("uva"); ADICIONA UM ELEMENTO A LISTA
+lista.pop(); REMOVE O ELEMENTO
+console.log(lista.length); IMPRIME A QUANTIDADE DE ITENS NA LISTA
+console.log(lista.length); IMPRIME OS ELEMENTOS AO CONTRÁRIO 
+console.log(lista.join(" - ")) ACRESCENTA UM ESPAÇO ENTRE OS ITENS DA LISTA */
+
+/* var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome); */
